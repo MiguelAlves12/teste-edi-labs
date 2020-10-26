d5ed57df9e6a9c71f0948973841226cf9773f1ba
MiguelAlves12 <44376805+MiguelAlves12@users.noreply.github.com>
2020-10-26 19:46:55 -0300

    Update convert_json_to_csv.py

--- intern-python-test-main-MiguelAlves/convert_json_to_csv.py
+++ intern-python-test-main-MiguelAlves/convert_json_to_csv.py
@@ -11,7 +11,7 @@ with open(pathCSVFile, "w",encoding="utf-8") as csvFile:
     csv_file = csv.writer(csvFile,delimiter="|")
     csv_file.writerow(["ID","Name","Description","Quantity","Value","Total"])
     for item in data["order"]:
-        price = (item["quantity"])*float(item["value"])
+        price = float(item["quantity"])*float(item["value"])
         total = price + total
         csv_file.writerow([item["id"],item["name"],item["description"],item["quantity"],item["value"],price])
     csv_file.writerow(["Total","","","","",total])
